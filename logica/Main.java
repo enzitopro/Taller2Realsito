a37605a1f122b10db0ebbf4a0092ffd425e94c5d
enzitopro <ajipicantemrdayt@gmail.com>
2026-04-16 14:19:11 -0400

    add txt, clase zonas y metodos para leer pokedex

--- logica/Main.java
+++ logica/Main.java
@@ -1,7 +1,41 @@
 package logica;
 
+import java.io.File;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.FileNotFoundException;
+
 public class Main {
+	public static ArrayList<Pokemon> listaPokedex = new ArrayList<>();
+	public static ArrayList<Zona> listaZonas = new ArrayList<>();
+	
 	public static void main(String[] args) {
+		leerPokedex();
+		System.out.println(listaPokedex.size());
+	}
+	
+	public static void leerPokedex() {
+		try {
+			File pokedex = new File("Pokedex.txt");
+			Scanner lector = new Scanner(pokedex);
+			while (lector.hasNextLine()) {
+				String lineaActual = lector.nextLine();
+				String[] partes = lineaActual.split(";");
+				double spawnChance = Double.parseDouble(partes[2]);
+				int vidaPoke = Integer.parseInt(partes[3]);
+				int ataquePoke = Integer.parseInt(partes[4]);
+				int defensaPoke = Integer.parseInt(partes[5]);
+				int ataqueEspPoke = Integer.parseInt(partes[6]);
+				int defensaEspPoke = Integer.parseInt(partes[7]);
+				int velocidadPoke = Integer.parseInt(partes[8]);
+				Pokemon nuevoPoke = new Pokemon(partes[0], partes[1], spawnChance, vidaPoke, ataquePoke, defensaPoke, ataqueEspPoke, defensaEspPoke, velocidadPoke, partes[9]);
+				listaPokedex.add(nuevoPoke);
+				
+			}
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("No se encontró el archivo de PokeDex.");
+		}
 		
 	}
 }
--- logica/Pokemon.java
+++ logica/Pokemon.java
@@ -1,5 +1,126 @@
 package logica;
 
 public class Pokemon {
+	private String nombre;
 
+	private String habitat;
+	private double porcentajeAparicion;
+	private int vida;
+	private int ataque;
+	private int defensa;
+	private int ataqueEspecial;
+	private int defensaEspecial;
+	private int velocidad;
+	private String tipo;
+	private String estado;
+
+	// constructor pokemon
+	public Pokemon(String nombre, String habitat, double porcentajeAparicion, int vida, int ataque, int defensa,
+			int ataqueEspecial, int defensaEspecial, int velocidad, String tipo) {
+		super();
+		this.nombre = nombre;
+		this.habitat = habitat;
+		this.porcentajeAparicion = porcentajeAparicion;
+		this.vida = vida;
+		this.ataque = ataque;
+		this.defensa = defensa;
+		this.ataqueEspecial = ataqueEspecial;
+		this.defensaEspecial = defensaEspecial;
+		this.velocidad = velocidad;
+		this.tipo = tipo;
+		this.estado = "Vivo";
+	}
+
+	// getters y setters jeje
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getHabitat() {
+		return habitat;
+	}
+
+	public double getPorcentajeAparicion() {
+		return porcentajeAparicion;
+	}
+
+	public int getVida() {
+		return vida;
+	}
+
+	public int getAtaque() {
+		return ataque;
+	}
+
+	public int getDefensa() {
+		return defensa;
+	}
+
+	public int getAtaqueEspecial() {
+		return ataqueEspecial;
+	}
+
+	public int getDefensaEspecial() {
+		return defensaEspecial;
+	}
+
+	public int getVelocidad() {
+		return velocidad;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setHabitat(String habitat) {
+		this.habitat = habitat;
+	}
+
+	public void setPorcentajeAparicion(double porcentajeAparicion) {
+		this.porcentajeAparicion = porcentajeAparicion;
+	}
+
+	public void setVida(int vida) {
+		this.vida = vida;
+	}
+
+	public void setAtaque(int ataque) {
+		this.ataque = ataque;
+	}
+
+	public void setDefensa(int defensa) {
+		this.defensa = defensa;
+	}
+
+	public void setAtaqueEspecial(int ataqueEspecial) {
+		this.ataqueEspecial = ataqueEspecial;
+	}
+
+	public void setDefensaEspecial(int defensaEspecial) {
+		this.defensaEspecial = defensaEspecial;
+	}
+
+	public void setVelocidad(int velocidad) {
+		this.velocidad = velocidad;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	public int getSumaStats() {
+		return vida+ataque+defensa+ataqueEspecial+defensaEspecial+velocidad;
+	}
 }
