6988618a38a877a1139b5a9d943a451acf6e7d5b
enzitopro <ajipicantemrdayt@gmail.com>
2026-04-16 17:14:08 -0400

    add finalizar clase gimnasio y tweaks al main

--- logica/Gimnasio.java
+++ logica/Gimnasio.java
@@ -1,5 +1,37 @@
 package logica;
 
-public class Gimnasio {
+import java.util.ArrayList;
 
+public class Gimnasios {
+	private int numeroGimnasio;
+	private String nombreLider;
+	private String estadoLider;
+	private ArrayList<Pokemon> equipoLider;
+
+	public Gimnasios(int numeroGimnasio, String nombreLider, String estadoLider) {
+		this.numeroGimnasio = numeroGimnasio;
+		this.nombreLider = nombreLider;
+		this.estadoLider = estadoLider;
+		this.equipoLider = new ArrayList<>();
+	}
+
+	public int getNumeroGimnasio() {
+		return numeroGimnasio;
+	}
+
+	public String getNombreLider() {
+		return nombreLider;
+	}
+
+	public String getEstadoLider() {
+		return estadoLider;
+	}
+
+	public void setEstadoLider(String estadoLider) {
+		this.estadoLider = estadoLider;
+	}
+
+	public ArrayList<Pokemon> getEquipoLider() {
+		return equipoLider;
+	}
 }
--- logica/Main.java
+++ logica/Main.java
@@ -8,13 +8,23 @@ import java.io.FileNotFoundException;
 public class Main {
 	public static ArrayList<Pokemon> listaPokedex = new ArrayList<>();
 	public static ArrayList<Zona> listaZonas = new ArrayList<>();
-	public static ArrayList<Gimnasio> listaGimnasios = new ArrayList<>();
-	
+	public static ArrayList<Gimnasios> listaGimnasios = new ArrayList<>();
+	public static ArrayList<AltoMando> listaAltosMandos = new ArrayList<>();
+
 	public static void main(String[] args) {
 		leerPokedex();
 		System.out.println(listaPokedex.size());
 	}
-	
+
+	public static Pokemon buscarEnPokedex(String nombrePokemon) {
+		for (Pokemon poke : listaPokedex) {
+			if (poke.getNombre().equalsIgnoreCase(nombrePokemon))
+				;
+			return poke;
+		}
+		return null;
+	}
+
 	public static void leerPokedex() {
 		try {
 			File pokedex = new File("Pokedex.txt");
@@ -29,25 +39,17 @@ public class Main {
 				int ataqueEspPoke = Integer.parseInt(partes[6]);
 				int defensaEspPoke = Integer.parseInt(partes[7]);
 				int velocidadPoke = Integer.parseInt(partes[8]);
-				Pokemon nuevoPoke = new Pokemon(partes[0], partes[1], spawnChance, vidaPoke, ataquePoke, defensaPoke, ataqueEspPoke, defensaEspPoke, velocidadPoke, partes[9]);
+				Pokemon nuevoPoke = new Pokemon(partes[0], partes[1], spawnChance, vidaPoke, ataquePoke, defensaPoke,
+						ataqueEspPoke, defensaEspPoke, velocidadPoke, partes[9]);
 				listaPokedex.add(nuevoPoke);
-				
 			}
-			
+			lector.close();
+
 		} catch (FileNotFoundException e) {
 			System.out.println("No se encontró el archivo de PokeDex.");
 		}
-		
 	}
-	
-	public static Pokemon buscarEnPokedex(String nombrePokemon) {
-        for (Pokemon poke : listaPokedex) {
-            if (poke.getNombre().equalsIgnoreCase(nombrePokemon));
-            return poke;
-        }
-        return null;
-	}
-	
+
 	public static void leerHabitats() {
 		try {
 			File habitats = new File("Habitats.txt");
@@ -57,8 +59,66 @@ public class Main {
 				Zona nuevaZona = new Zona(zonaLeida);
 				listaZonas.add(nuevaZona);
 			}
+			lector.close();
 		} catch (FileNotFoundException e) {
 			System.out.println("No se encontro el archivo de Habitats.");
+		}
+	}
+
+	public static void leerGimnasios() {
+		try {
+			File gimnasio = new File("Gimnasios.txt");
+			Scanner lector = new Scanner(gimnasio);
+			while (lector.hasNextLine()) {
+				String lineaActual = lector.nextLine();
+				String[] partes = lineaActual.split(";");
+				int numeroGimnasio = Integer.valueOf(partes[0]);
+				String nombreLider = partes[1];
+				String estadoLider = partes[2];
+				int cantPokemons = Integer.valueOf(partes[3]);
+
+				Gimnasios nuevoGim = new Gimnasios(numeroGimnasio, nombreLider, estadoLider);
+
+				for (int i = 0; i < cantPokemons; i++) {
+					String nombrePokemonLider = partes[4 + i];
+					Pokemon pokeEncontrado = buscarEnPokedex(nombrePokemonLider);
+					if (pokeEncontrado != null) {
+						nuevoGim.getEquipoLider().add(pokeEncontrado);
+					}
+				}
+				listaGimnasios.add(nuevoGim);
+			}
+			lector.close();
+
+		} catch (FileNotFoundException e) {
+			System.out.println("No se encontró el archivo de Gimnasios.");
+		}
+	}
+
+	public static void leerAltoMando() {
+		try {
+			File altoMando = new File("Alto Mando.txt");
+			Scanner lector = new Scanner(altoMando);
+			while (lector.hasNextLine()) {
+				String lineaActual = lector.nextLine();
+				String[] partes = lineaActual.split(";");
+				int numeroAltoMando = Integer.valueOf(partes[0]);
+				String nombreAltoMando = partes[1];
+
+				AltoMando nuevoAltoMando = new AltoMando(numeroAltoMando, nombreAltoMando);
+
+				for (int i = 0; i < 6; i++) {
+					String nombrePokemonAltoMando = partes[2 + i];
+					Pokemon pokeEncontrado = buscarEnPokedex(nombrePokemonAltoMando);
+					if (pokeEncontrado != null) {
+						nuevoAltoMando.getEquipoAltoMando().add(pokeEncontrado);
+					}
+				}
+				listaAltosMandos.add(nuevoAltoMando);
+			}
+			lector.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("No se encontró el archivo de Alto Mando.");
 		}
 	}
 }
