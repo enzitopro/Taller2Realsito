feb4bac0c457b6d4fc0683447ec5e1e3a8d64c87
enzitopro <ajipicantemrdayt@gmail.com>
2026-04-16 17:16:36 -0400

    add logica altomando

--- logica/AltoMando.java
+++ logica/AltoMando.java
@@ -1,5 +1,27 @@
 package logica;
 
-public class AltoMando {
+import java.util.ArrayList;
 
+public class AltoMando {
+	private int numeroAltoMando;
+	private String nombreAltoMando;
+	private ArrayList<Pokemon> equipoAltoMando;
+	
+	public AltoMando(int numeroAltoMando, String nombreAltoMando) {
+		this.numeroAltoMando = numeroAltoMando;
+		this.nombreAltoMando = nombreAltoMando;
+		this.equipoAltoMando = new ArrayList<>();
+	}
+
+	public int getNumeroAltoMando() {
+		return numeroAltoMando;
+	}
+
+	public String getNombreAltoMando() {
+		return nombreAltoMando;
+	}
+	
+	public ArrayList<Pokemon> getEquipoAltoMando() {
+		return equipoAltoMando;
+	}
 }
